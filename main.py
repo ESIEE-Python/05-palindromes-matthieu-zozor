e6572efd1408275e826bfc4d606796e4f812a32b
matthieu-zozor <matthieu.zozor@edu.esiee.fr>
2024-09-16 15:27:49 +0000

    version finale optimale de ispalindrome

--- main.py
+++ main.py
@@ -16,31 +16,17 @@ def ispalindrome(p):
     )
     p=p.lower() # regler le pb des majuscules
     p=p.translate(table)#regle le pb des accents
-    a=0
-    b=-1
+    p=p.replace(" ","")
     rg=0 #variable pour faire le moins d'iteration possible sur p
     if len(p)%2==0: #si pair on met la moitié du mot en iteration
         rg=len(p)//2
     else:
         rg=len(p)//2+1 #si impair, la moitié +1 pour le caractere du milieu
+
     for i in range(rg):
-        #print(f"test numero {i}:{p[a]}!={p[b]}",a,b) #pour observer chaque iteration
-        if p[a]!=p[b]: # si different
-            if p[a]==" ":         # si different car espace 
-                while p[a]==" ":  # saute tous les espaces
-                    a+=1
-                    #print(f"p[a]={p[a]}")
-            elif p[b]==" ":
-                while p[b]==" ":  #pareil saute espaces
-                    b-=1
-                    #print(f"p[b]={p[b]}")
-            elif p[a]!=p[b]:
-                return False
-            else:  # si pas different a cause des espaces
-                #print(f"{p[a]}!={p[b]}",a,b)
-                return False
-        a+=1
-        b-=1
+        #print(f"test numero {i}:{p[i]}!={p[-i-1]}",i,-i-1) #pour observer chaque iteration
+        if p[i]!=p[-i-1]:
+            return False
     return True
 
 #### Fonction principale
